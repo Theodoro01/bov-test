cff732ab8ae0c6db2ebc385d45c48a8b353c0a60
Theodoro01 <thedorovieira01@gmail.com>
2022-08-10 17:05:29 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// import express from 'express'
+// import Routes from './src/routes/routes.js'
+// import dotenv from 'dotenv'
+// import database from './src/database/database.js'
+
+// database.connectToDatabase()
+
+// dotenv.config()
+
+// const app = express()
+
+// app.use(express.json())
+// app.use('/v1', Routes)
+
+// app.route('/healthcheck').get((_, res) => res.status(200).json({ msg: 'OK' }))
+
+// app.all('*', (_, res) => res.status(404).json({ message: 'Not found!' }))
+
+// app.listen(process.env.PORT, () => {
+//   console.log('Hello World!')
+// })
