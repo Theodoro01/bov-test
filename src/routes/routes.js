312e9beb27a259e6ea2d7cda0e700723c5756ad9
Theodoro01 <thedorovieira01@gmail.com>
2022-08-11 11:27:01 -0300

    route register created

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import UserController from '../controllers/UserController.js'
 
 const routes = express.Router()
 
 routes.get('/home', (_, res) => res.status(200).json({ msg: 'ok' }))
+routes.post('/register', UserController.register)
 
 export default routes
