10638fc518b3ce33b88f7f11231eab7587100c6f
Theodoro01 <thedorovieira01@gmail.com>
2022-08-11 15:21:12 -0300

    Validation with AJV ( Login Router )

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import UserController from '../controllers/UserController.js'
 import validateRegister from '../middlewares/validateRoutes/validateRegister.js'
+import validateLogin from '../middlewares/validateRoutes/validateLogin.js'
 const routes = express.Router()
 
 routes.get('/home', (_, res) => res.status(200).json({ msg: 'ok' }))
 routes.post('/register', validateRegister.execute, UserController.register)
-routes.post('/login', UserController.login)
+routes.post('/login', validateLogin.execute, UserController.login)
 
 export default routes
