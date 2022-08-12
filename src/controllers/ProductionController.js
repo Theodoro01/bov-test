bc3b4e331f8a6619afc7890cf91be471aee987ae
Theodoro01 <thedorovieira01@gmail.com>
2022-08-12 12:31:53 -0300

    Route searchAllProduction created

--- src/controllers/ProductionController.js
+++ src/controllers/ProductionController.js
@@ -1,4 +1,5 @@
 import createProductionRepository from '../database/repository/production/createProductionRepository.js'
+import searchProductionRepository from '../database/repository/production/searchProductionRepository.js'
 
 export default {
   insertProduction: async (req, res) => {
@@ -8,6 +9,18 @@ export default {
       return res.status(201).json(resultCreate)
     } catch (error) {
       return res.status(500).json({ error: 'Registration failed' })
+    }
+  },
+  allProduction: async (req, res) => {
+    try {
+      const { year, month, farmCod } = req.body
+      const result = await searchProductionRepository.execute(year, month, farmCod)
+      if (result?.totalRegister === 0) {
+        return res.status(404).json({ error: 'production not found' })
+      }
+      return res.status(200).json(result)
+    } catch (error) {
+      return res.status(500).json({ error })
     }
   }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,8 @@ import validateLogin from '../middlewares/validateRoutes/validateLogin.js'
 import validateAuth from '../middlewares/token/auth.js'
 import validateFarm from '../middlewares/validateRoutes/validateFarm.js'
 import validateInsertProduction from '../middlewares/validateRoutes/validateInsertProduction.js'
+import validateSearchProduction from '../middlewares/validateRoutes/validateSearchProduction.js'
+
 const routes = express.Router()
 
 routes.get('/home', (_, res) => res.status(200).json({ msg: 'ok' }))
@@ -16,5 +18,6 @@ routes.post('/login', validateLogin.execute, UserController.login)
 routes.post('/farmRegister', validateAuth.auth, validateFarm.execute, FarmController.farmRegister)
 routes.get('/searchAllFarms', validateAuth.auth, FarmController.searchAllFarms)
 routes.post('/insertProduction', validateAuth.auth, validateInsertProduction.execute, ProductionController.insertProduction)
+routes.get('/searchAllProduction', validateAuth.auth, validateSearchProduction.execute, ProductionController.allProduction)
 
 export default routes
