6ff816da81da19e994fe3f56fa04da8e5a2c1b10
Theodoro01 <thedorovieira01@gmail.com>
2022-08-12 15:55:45 -0300

    Validation with Ajv ( Milk Year Avg Production )

---
+++ src/middlewares/validateRoutes/validateAvgYearProduction.js
@@ -0,0 +1,19 @@
+import Ajv from 'ajv'
+
+import validateAvgYearProduction from '../validateSchema/avgYearProductionSchema.js'
+
+const ajv = new Ajv({ allErrors: true })
+
+export default {
+  execute: (req, res, next) => {
+    const production = req.body
+    const productionValidate = ajv.compile(validateAvgYearProduction)
+    const validateproduction = productionValidate(production)
+
+    if (!validateproduction) {
+      return res.status(400).json({ message: productionValidate.errors })
+    }
+
+    return next()
+  }
+}
