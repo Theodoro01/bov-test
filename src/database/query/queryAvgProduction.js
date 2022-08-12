37dd25581b7c0055f7cf9254605efaab77f30abc
Theodoro01 <thedorovieira01@gmail.com>
2022-08-12 15:35:16 -0300

    Query Avg Production

---
+++ src/database/query/queryAvgProduction.js
@@ -0,0 +1,18 @@
+export default {
+  execute: async (dateYear, dateMonth, farmCod) => {
+    return [
+      { $match: { farmCod } },
+      {
+        $match: {
+          $and: [{ createdAt: { $gte: new Date(dateYear, dateMonth - 1, 1) } },
+            { createdAt: { $lte: new Date(dateYear, dateMonth, 0) } }]
+        }
+      }, {
+        $group: {
+          _id: '',
+          avg: { $avg: '$diaryMilkProduction' }
+        }
+      }
+    ]
+  }
+}
